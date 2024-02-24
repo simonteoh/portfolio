a5ff89ef076a808f614beaffa36d1384dda9810d
simon <simonteoh1996@gmail.com>
2024-02-24 17:01:36 +0800

    add key to Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -45,9 +45,9 @@ export default function Nav() {
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        {links.map((link) => {
+                        {links.map((link, index) => {
                             return (
-                                <li>
+                                <li key={index}>
                                     <Link href={link.url} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                                     >{link.name}</Link>
                                 </li>
